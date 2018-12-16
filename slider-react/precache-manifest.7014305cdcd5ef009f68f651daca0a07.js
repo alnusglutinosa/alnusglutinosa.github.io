54baa46f2ff9183e09d97cf5ab9eaf87ac208213
alnusglutinosa <41738196+alnusglutinosa@users.noreply.github.com>
2018-12-16 21:10:46 +0200

    1
    1

---
+++ slider-react/precache-manifest.7014305cdcd5ef009f68f651daca0a07.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "6fe0086df66b03d957946df732b034fa",
+    "url": "/static/media/slide-4.6fe0086d.jpg"
+  },
+  {
+    "revision": "ba47c1524506888763e995c032c0db53",
+    "url": "/static/media/slide-3.ba47c152.jpg"
+  },
+  {
+    "revision": "7cd21dbdab4d400374cfc664fdb0683a",
+    "url": "/static/media/slide-2.7cd21dbd.jpg"
+  },
+  {
+    "revision": "51908206770f604764e9e4365c4f6a13",
+    "url": "/static/media/slide-1.51908206.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8108b8c30fee1973d104",
+    "url": "/static/js/main.8108b8c3.chunk.js"
+  },
+  {
+    "revision": "066a362ad9f09bc8e349",
+    "url": "/static/js/1.066a362a.chunk.js"
+  },
+  {
+    "revision": "8108b8c30fee1973d104",
+    "url": "/static/css/main.afe96f89.chunk.css"
+  },
+  {
+    "revision": "b0e2d6a54e1e7f57bfd2bc248baab984",
+    "url": "/index.html"
+  }
+];
